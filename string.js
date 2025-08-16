14b9f8ddf159747451ee5b5f471fdcdf9f903b1a
ovijet halder <ovijethalder143@gmail.com>
2025-08-16 10:19:50 +0600

    string

---
+++ string.js
@@ -0,0 +1,14 @@
+let promise =
+  "ami promise korci je ami web development sika amr dream ke sotti korte chai";
+
+// console.log(promise.toUpperCase());
+console.log(promise.indexOf("web"));
+
+console.log(promise.split(" "));
+// split er output  [  'ami',         'promise',
+//   'korci',       'je',
+//   'ami',         'web',
+//   'development', 'sika',
+//   'amr',         'dream',
+//   'ke',          'sotti',
+//   'korte',       'chai']
