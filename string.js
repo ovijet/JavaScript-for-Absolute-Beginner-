a602517a3666ff9f4e57e4188bc160441472e480
ovijet halder <ovijethalder143@gmail.com>
2025-08-17 00:15:25 +0600

    math

--- string.js
+++ string.js
@@ -12,3 +12,16 @@ console.log(promise.split(" "));
 //   'amr',         'dream',
 //   'ke',          'sotti',
 //   'korte',       'chai']
+
+let a = 22.55;
+let b = 25.5;
+
+console.log(parseFloat(a));
+console.log(parseInt(b));
+
+let num1 = 25;
+let num2 = "ovi";
+num1++;
+let result = num1 + " " + num2;
+
+console.log(result);
