59ea2bbaeaac77dfd08e8d6f7934db5e615b8e63
ovijet halder <ovijethalder143@gmail.com>
2025-08-18 00:10:43 +0600

    js-code

---
+++ date.js
@@ -0,0 +1,7 @@
+var date = new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.getDate());
+console.log(date.getFullYear());
+
+
---
+++ operator.js
@@ -0,0 +1,12 @@
+let a = 20;
+let b = `10`;
+
+// a += b;
+// a -=b
+// console.log(a);
+
+console.log(a === b);
+console.log(a <= b);
+console.log(a < !b);
+console.log(a >= b);
+
