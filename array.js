37756e1aaa6abe6ae34cd4647a462e62ad1e82c2
ovijet halder <ovijethalder143@gmail.com>
2025-08-19 00:08:42 +0600

    array

---
+++ array.js
@@ -0,0 +1,17 @@
+// let a = [10, 20, 11, 30, 43];
+
+// a.push(20);
+// // a.pop()
+// console.log(a.length);
+
+// console.log(a[3]);
+
+
+let arr = [1,2,3,4,5,6]
+
+for(let i=0;i<arr.length;i++){
+    if (arr[i]%3==0) {
+        console.log(arr[i]);
+    }
+}
+
