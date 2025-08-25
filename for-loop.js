371a54b5d69f5914f7987d20efaf5a090a535665
ovijet halder <ovijethalder143@gmail.com>
2025-08-25 23:45:48 +0600

    for-loop

--- for-loop.js
+++ for-loop.js
@@ -2,9 +2,32 @@
 //   console.log(i);
 // }
 
-let arr = ["ovi, payel, koyel, riya"];
+// let arr = ["ovi, payel, koyel, riya"];
 
-for (let i = 0; i < arr.length; i++) {
-  let element = arr[i];
-  console.log(element);
+// for (let i = 0; i < arr.length; i++) {
+//   let element = arr[i];
+//   console.log(element);
+// }
+
+// let n = 10;
+// for (let i = 1; i <= n; i++) {
+//   let result = "";
+//   for (let j = 1; j <= i; j++) {
+//     result += j + "";
+//   }
+//   console.log(result);
+// }
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i <= 10; i++) {
+//   if (i % 2) {
+//     console.log(i);
+//   }
+// }
+
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 3) {
+    console.log(i);
+  }
 }
